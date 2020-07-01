f39e19c11c351a3160ff9aa212473b734e677694
yoshi-automation <yoshi-automation@google.com>
2020-07-01 07:22:35 -0700

    This is first release of the API protos and Bazel BUILD files for the Analytics Admin API V1alpha.
    PiperOrigin-RevId: 319180144
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Wed Jul 1 01:09:17 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: e6ab0a55f2195169feded73dd684574dd4bd9dfa
    Source-Link: https://github.com/googleapis/googleapis/commit/e6ab0a55f2195169feded73dd684574dd4bd9dfa

--- google/cloud/recommender_v1beta1/proto/recommender_service_pb2.py
+++ google/cloud/recommender_v1beta1/proto/recommender_service_pb2.py
@@ -1198,6 +1198,7 @@ ListRecommendationsRequest = _reflection.GeneratedProtocolMessageType(
           Filter expression to restrict the recommendations returned.
           Supported filter fields: state_info.state Eg:
           \`state_info.state:“DISMISSED” or state_info.state:“FAILED”
+          .. [1]    a-z0-9  .. [2]    a-z0-9  .. [3]    a-z0-9
   """,
         # @@protoc_insertion_point(class_scope:google.cloud.recommender.v1beta1.ListRecommendationsRequest)
     },
@@ -1299,12 +1300,11 @@ MarkRecommendationSucceededRequest = _reflection.GeneratedProtocolMessageType(
       state_metadata:
           State properties to include with this state. Overwrites any
           existing ``state_metadata``. Keys must match the regex ``/
-          [3]_[a-z0-9_.-]{0,62}/``. Values must match the regex
+          [2]_[a-z0-9_.-]{0,62}/``. Values must match the regex
           ``/^[a-zA-Z0-9_./-]{0,255}/``.
       etag:
           Required. Fingerprint of the Recommendation. Provides
-          optimistic locking.  .. [1]    a-z0-9  .. [2]    a-z0-9  ..
-          [3]    a-z0-9
+          optimistic locking.
   """,
         # @@protoc_insertion_point(class_scope:google.cloud.recommender.v1beta1.MarkRecommendationSucceededRequest)
     },
@@ -1335,7 +1335,7 @@ MarkRecommendationFailedRequest = _reflection.GeneratedProtocolMessageType(
       state_metadata:
           State properties to include with this state. Overwrites any
           existing ``state_metadata``. Keys must match the regex ``/
-          [2]_[a-z0-9_.-]{0,62}/``. Values must match the regex
+          [3]_[a-z0-9_.-]{0,62}/``. Values must match the regex
           ``/^[a-zA-Z0-9_./-]{0,255}/``.
       etag:
           Required. Fingerprint of the Recommendation. Provides
