e93e53298664f083244eb2cfcfbc75e4c9e8a7c3
yoshi-automation <yoshi-automation@google.com>
2020-07-14 06:29:38 -0700

    chore: add Node.js package name to BigQuery Reservation build file
    PiperOrigin-RevId: 320300472
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Wed Jul 8 17:40:33 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 4f4aa3a03e470f1390758b9d89eb1aa88837a5be
    Source-Link: https://github.com/googleapis/googleapis/commit/4f4aa3a03e470f1390758b9d89eb1aa88837a5be

--- google/cloud/recommender_v1beta1/proto/recommender_service_pb2.py
+++ google/cloud/recommender_v1beta1/proto/recommender_service_pb2.py
@@ -1112,8 +1112,7 @@ ListInsightsResponse = _reflection.GeneratedProtocolMessageType(
           The set of insights for the ``parent`` resource.
       next_page_token:
           A token that can be used to request the next page of results.
-          This field is empty if there are no additional results.  ..
-          [1]    a-z0-9  .. [2]    a-z0-9  .. [3]    a-z0-9
+          This field is empty if there are no additional results.
   """,
         # @@protoc_insertion_point(class_scope:google.cloud.recommender.v1beta1.ListInsightsResponse)
     },
@@ -1218,7 +1217,8 @@ ListRecommendationsResponse = _reflection.GeneratedProtocolMessageType(
           The set of recommendations for the ``parent`` resource.
       next_page_token:
           A token that can be used to request the next page of results.
-          This field is empty if there are no additional results.
+          This field is empty if there are no additional results.  ..
+          [1]    a-z0-9  .. [2]    a-z0-9  .. [3]    a-z0-9
   """,
         # @@protoc_insertion_point(class_scope:google.cloud.recommender.v1beta1.ListRecommendationsResponse)
     },
